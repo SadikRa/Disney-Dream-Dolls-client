d97899e3638d87b11d8cf4cfb45435f213e462c8
SadikRa <sadikrahman494@gmail.com>
2023-05-20 10:39:31 +0600

    assessment all most done

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -20,6 +20,11 @@ const Home = () => {
                 <h1 className="text-5xl text-center my-5">Buy Your favorite Toys</h1>
                 <ShopToy></ShopToy>
             </div>
+            <div className="mb-12">
+                <h1 className="text-4xl my-5">Share Your Opinions</h1>
+                <textarea className="w-full rounded" name="" id="" cols="30" rows="10"></textarea><br />
+                <button className="btn btn-primary">Submit</button>
+            </div>
         </div>
     );
 };
